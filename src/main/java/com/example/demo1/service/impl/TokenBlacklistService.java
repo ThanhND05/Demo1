8cf056a8b948ba4cc392148f88e17634ecaec2d5
ThanhND05 <ThanhND.B23CN761@stu.ptit.edu.vn>
2025-10-04 11:57:10 +0700

    backend

---
+++ src/main/java/com/example/demo1/service/impl/TokenBlacklistService.java
@@ -0,0 +1,19 @@
+package com.example.demo1.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class TokenBlacklistService {
+    private Set<String> blacklist = new HashSet<>();
+
+    public void blacklistToken(String token) {
+        blacklist.add(token);
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        return blacklist.contains(token);
+    }
+}
